fc576b6e33da175c2e6eac23871e0672c57642d2
yyz <754628891@qq.com>
2019-01-15 12:07:30 +0800

    feat: app.py add index, open to all host visit.

--- app.py
+++ app.py
@@ -1,11 +1,31 @@
+import os
+import re
+import platform
+
 from flask import Flask
+from utils.logger import get_logger, get_flask_log_handler
+from utils.config import get_config
+
 app = Flask(__name__)
 
+
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    string = get_config()['database']['url']
+    if string is None:
+        db_path = "No Database configuration..."
+    else:
+        db_path = re.search('(?<=@).*', string).group(0)
+    return """
+        <h1>Server started...</h1>
+        <h2>Python version: %s<h2>
+        <h2>Database: %s</h2>
+    """ % (platform.python_version(), db_path)
+
 
 
 if __name__ == '__main__':
- app.run()
+    get_logger().info("App started...")
+    app.run(host='0.0.0.0', debug=True)
 
