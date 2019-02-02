a3a85a5ba8706840f651cb1616da309bb40f7023
yyz <754628891@qq.com>
2019-02-02 11:28:27 +0800

    feat:add user upload.

--- blueprints/user/services/login_service.py
+++ blueprints/user/services/login_service.py
@@ -2,16 +2,16 @@ from flask import session
 
 from models.ACC_USER import AccUser
 from utils.db_connection import DbSession
-
+from utils.errors.parameter_errors import BadRequest
 
 def login(username, password):
     with DbSession() as db_session:
         register_user = db_session.query(AccUser).filter(AccUser.username==username).first()
         if register_user is None:
-            return ;
+            raise BadRequest(1, 'No such user')
         else:
             if register_user.password!=password:
-                return None
+                raise BadRequest(2, 'Password error')
             else:
                 userid=register_user.id;
                 session['userid']=userid
--- blueprints/user/user.py
+++ blueprints/user/user.py
@@ -1,10 +1,10 @@
 #coding:utf-8
 import logging
 import random
-
+import os
 import hashlib
-from flask import Blueprint, session, request, render_template
-
+from flask import Blueprint, session, request, render_template, redirect, url_for
+from werkzeug.utils import secure_filename
 from blueprints.user.services import login_service
 
 from utils.errors.success import succ_json
@@ -13,6 +13,7 @@ from utils.object_attr_ops import mask_pass
 
 
 routes = Blueprint('user', __name__, template_folder='templates')
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 @routes.route('/login', methods=['GET', 'POST'])
 def login():
@@ -22,13 +23,17 @@ def login():
         user_id, user_info = login_service.login(username, password)
         return succ_json(mask_pass(user_info))
     else:
-        return render_template(
-            'login/login.html'
-        )
+        return '''
+                <form action="" method="POST">
+                    <p><input type=text name=username>
+                    <p><input type=text name=password>
+                    <p><input type=submit value=Login>
+                </form>
+            '''
 
 @routes.route('success')
 def login_success():
-    return render_template('success.html')
+    return render_template('/success/success.html')
 
 @routes.route('index')
 def index():
@@ -38,3 +43,28 @@ def index():
         title='Welcome',
         user=user
     )
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+
+@routes.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join('/home/yyz/PycharmProjects/AccountApp/upload', filename))
+            return redirect(url_for('user.upload_file',
+                                    filename=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form action="" method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
+
